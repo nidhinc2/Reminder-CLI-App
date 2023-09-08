3c8c4001f2e42e3286e32f6f6393fd7fccdc2d3d
Nidhi Chaudhari <73796485+nidhinc2@users.noreply.github.com>
2023-09-08 12:51:15 -0500

    Update send_email.py

--- send_email.py
+++ send_email.py
@@ -1,5 +1,5 @@
-#import schedule
-
+import schedule
+import time
 import smtplib, ssl
 import getpass
 from email.message import EmailMessage
@@ -11,17 +11,26 @@ def send_email():
     sender_password = getpass.getpass(prompt = "Sender email Password : ")
     #sender_password = input("Sender email Password : ")
     receiver_email = input("Receiver Email : ")
+    mail_subject = input("Set the subject of your email")
+    mail_content = input("Send the content of your email")
 
     # Create the email content
     mail = EmailMessage()
     mail["From"] = sender_email
     mail["To"] = receiver_email
-    mail["Subject"] = "Test Email"
-    mail.set_content(" Hi Nidhi, just testing if my code works!!  ")
+    mail["Subject"] = mail_subject
+    mail.set_content(mail_content)
     
     context = ssl.create_default_context()
 
     with smtplib.SMTP_SSL("smtp.gmail.com", port, context=context) as server:
         server.login(send_emailer, sender_password)
         server.sendmail(sender_email, receiver_email, mail.as_string())
-send_email()        
+
+
+time_w = input("What time do you want to schedule it: ") 
+schedule.every().day.at(time_w).do(send_email)
+
+while True:
+    schedule.run_pending()
+    time.sleep(1)    
